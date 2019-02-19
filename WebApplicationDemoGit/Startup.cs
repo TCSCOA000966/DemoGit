7f076f32c177c46b75a2ba5488f534545e0666a4
TCSCOA000966 <TCSCOA000966@SASV04CN>
2019-02-19 19:06:33 +0100

    Add project files.

---
+++ WebApplicationDemoGit/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationDemoGit.Startup))]
+namespace WebApplicationDemoGit
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
